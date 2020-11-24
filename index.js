f0b565bd9f8341c0bfb2b43c4490a97ea6a1e63c
Viacheslav Kulyk <vkulyk@zoolatech.com>
2020-11-24 20:43:19 +0200

    init

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ class RSA {
   static randomPrime(bits) {
     const min = bigInt.one.shiftLeft(bits - 1);
     const max = bigInt.one.shiftLeft(bits).prev();
-    
+
     while (true) {
       let p = bigInt.randBetween(min, max);
       if (p.isProbablePrime(256)) {
         return p;
-      } 
+      }
     }
   }
 
@@ -18,18 +18,22 @@ class RSA {
     let p;
     let q;
     let totient;
-  
+
     do {
       p = this.randomPrime(keysize / 2);
       q = this.randomPrime(keysize / 2);
-      totient = bigInt.lcm(
-        p.prev(),
-        q.prev()
-      );
-    } while (bigInt.gcd(e, totient).notEquals(1) || p.minus(q).abs().shiftRight(keysize / 2 - 100).isZero());
+      totient = bigInt.lcm(p.prev(), q.prev());
+    } while (
+      bigInt.gcd(e, totient).notEquals(1) ||
+      p
+        .minus(q)
+        .abs()
+        .shiftRight(keysize / 2 - 100)
+        .isZero()
+    );
 
     return {
-      e, 
+      e,
       n: p.multiply(q),
       d: e.modInv(totient),
     };
@@ -40,13 +44,13 @@ class RSA {
   }
 
   static decrypt(encryptedMsg, d, n) {
-    return bigInt(encryptedMsg).modPow(d, n); 
+    return bigInt(encryptedMsg).modPow(d, n);
   }
 
   static encode(str) {
     const codes = str
       .split('')
-      .map(i => i.charCodeAt())
+      .map((i) => i.charCodeAt())
       .join('');
 
     return bigInt(codes);
@@ -58,7 +62,7 @@ class RSA {
 
     for (let i = 0; i < stringified.length; i += 2) {
       let num = Number(stringified.substr(i, 2));
-      
+
       if (num <= 30) {
         string += String.fromCharCode(Number(stringified.substr(i, 3)));
         i++;
